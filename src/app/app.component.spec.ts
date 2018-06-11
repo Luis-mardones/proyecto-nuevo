576d47ded3513d324015e10fb5c9292a36d61c8b
Luis-mardones <luis.mardones2017@twk.cl>
2018-06-11 12:35:59 -0400

    6.1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to otro!');
+    expect(compiled.querySelector('h1').textContent).toContain('Bienvenido al club de la pelea!');
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http, Response} from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'al club de la pelea';
+  dateInspect = '';
+    valueUf = '';
+
+    constructor(private http: Http) {}
+
+     searchIndicator() {
+       this.http.get('https://mindicador.cl/api/uf/' + this.dateInspect)
+       .subscribe(
+           (res: Response) => {
+             const valueIndicator = res.json();
+             console.log(valueIndicator);
+             //this.valueUf =  valueIndicator.serie.valor; // res['serie'][0]['valor'];
+             this.valueUf = valueIndicator['serie'][0]['valor'];
+             //for(let miS of valueIndicator['serie']) {
+             //  console.log(miS);
+              // console.log(miS['valor']);
+            // }
+           }, err => {
+             console.log('UPS!');
+             console.log(err);
+           }, () => {
+             console.log('¡Servicio Finalizado!');
+             // https://angular.io/guide/quickstart
+             // https://materializecss.com/
+           }
+       );
+     }
+
+
 }
